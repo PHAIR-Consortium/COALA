1c4de3d91e62e11baa3bdc94e8d77ac64c7a39d2
adsitter <138677971+adsitter@users.noreply.github.com>
2025-08-22 15:48:17 +0200

    logging

--- nnUNet/nnunetv2/utilities/find_class_by_name.py
+++ nnUNet/nnunetv2/utilities/find_class_by_name.py
@@ -1,13 +1,21 @@
 import importlib
 import pkgutil
+import logging
+import sys
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+    stream=sys.stdout
+)
+logger = logging.getLogger(__name__)
 
 from batchgenerators.utilities.file_and_folder_operations import *
-
 
 def recursive_find_python_class(folder: str, class_name: str, current_module: str):
     tr = None
     for importer, modname, ispkg in pkgutil.iter_modules([folder]):
-        # print(modname, ispkg)
+        # logger.debug(f"Searching for class {class_name} in module {modname}")
         if not ispkg:
             m = importlib.import_module(current_module + "." + modname)
             if hasattr(m, class_name):
